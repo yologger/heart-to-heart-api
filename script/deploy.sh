00e0c6485966e14c8ac4498d79d4a8c85e47051c
yologger <yologger1013@gmail.com>
2022-01-27 18:22:53 +0900

    Test CI/CD

--- script/deploy.sh
+++ script/deploy.sh
@@ -17,10 +17,10 @@ else
   sleep 5
 fi
 
+echo "[$(date "+%Y-%m-%d %I:%M:%S")] JAR_NAME: ${JAR_NAME}" >> $REPOSITORY/deploy_log.txt
 echo "[$(date "+%Y-%m-%d %I:%M:%S")] Launch new application." >> $REPOSITORY/deploy_log.txt
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
-echo "[$(date "+%Y-%m-%d %I:%M:%S")] JAR_NAME: ${JAR_NAME}" >> $REPOSITORY/deploy_log.txt
 chmod +x $JAR_NAME
-nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" > /dev/null 2> /dev/null < /dev/null &
+nohup java sudo -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" > /dev/null 2> /dev/null < /dev/null &
 #nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" 2>&1 &
-# nohup java -Dspring.profiles.active=prod -jar /home/ec2-user/app/heart-to-heart-api/heart-to-heart-api-0.0.1.jar 2>&1 &
+# nohup java -Dspring.profiles.active=prod -jar /home/ec2-user/app/heart-to-heart-api/heart-to-heart-api-0.0.1.jar  > /dev/null 2> /dev/null < /dev/null &
