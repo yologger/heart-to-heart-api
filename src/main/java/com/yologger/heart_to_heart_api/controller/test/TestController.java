430c4fb9cb05401f486be87d1b97f843dd686da2
yologger <yologger1013@gmail.com>
2022-01-27 18:50:31 +0900

    Test CI/CD pipeline

--- src/main/java/com/yologger/heart_to_heart_api/controller/test/TestController.java
+++ src/main/java/com/yologger/heart_to_heart_api/controller/test/TestController.java
@@ -12,34 +12,4 @@ public class TestController {
     public String test1() {
         return "test1";
     }
-
-    @GetMapping("/test2")
-    public String test2() {
-        return "test2";
-    }
-
-    @GetMapping("/test3")
-    public String test3() {
-        return "test3";
-    }
-
-    @GetMapping("/test4")
-    public String test4() {
-        return "test4";
-    }
-
-    @GetMapping("/test5")
-    public String test5() {
-        return "test5";
-    }
-
-    @GetMapping("/test6")
-    public String test6() {
-        return "test6";
-    }
-
-    @GetMapping("/test7")
-    public String test7() {
-        return "test7";
-    }
 }
