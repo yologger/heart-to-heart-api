fa09776c42e231cc30d57c36aeaee11bf3e7d0d3
yologger <yologger1013@gmail.com>
2022-01-27 15:14:43 +0900

    Test CI/CD pipeline

--- script/deploy.sh
+++ script/deploy.sh
@@ -25,6 +25,11 @@ echo ">>> JAR_NAME: ${JAR_NAME}"
 nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" 2>&1
 
 ######
-ls -tr /home/ec2-user/app/heart-to-heart-api/ | grep jar | tail -n 1
 
-nohup java -Dspring.profiles.active=prod -jar /home/ec2-user/app/heart-to-heart-api/heart-to-heart-api-0.0.1 2>&1
+java -jar heart-to-heart-api-0.0.1.jar
+nohup java -jar heart-to-heart-api-0.0.1.jar 2>&1 &
+
+java -Dspring.profiles.active=prod -jar heart-to-heart-api-0.0.1.jar
+
+java -Dspring.config.location=classpath:/application.properties,classpath:/application-prod.properties -Dspring.profiles.active=prod -jar heart-to-heart-api-0.0.1.jar
+
