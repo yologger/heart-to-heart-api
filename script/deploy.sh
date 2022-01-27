10086885d91765dffc365c4589f71238d53b74a6
yologger <yologger1013@gmail.com>
2022-01-27 15:51:12 +0900

    Test CI/CD

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-echo "RESULT" > /home/ec2-user/app/heart-to-heart-api/test.txt
+REPOSITORY=/home/ec2-user/app/heart-to-heart-api
+PROJECT_NAME=heart-to-heart-api
 
-#REPOSITORY=/home/ec2-user/app/heart-to-heart-api
-#PROJECT_NAME=heart-to-heart-api
-#
-#echo ">>> Copy jar to project root."
-#cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
-#
-#CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
-#
-#if [ -z "$CURRENT_PID" ]; then
-#  echo ">>> There is no previously running application."
-#else
-#  echo ">>> A previously running application exists."
-#  echo ">>> kill -15 $$CURRENT_PID"
-#  kill -15 $$CURRENT_PID
-#  sleep 5
-#fi
+echo ">>> Copy jar to project root."
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+
+if [ -z "$CURRENT_PID" ]; then
+  echo ">>> There is no previously running application."
+else
+  echo ">>> A previously running application exists."
+  echo ">>> kill -15 $$CURRENT_PID"
+  kill -15 $$CURRENT_PID
+  sleep 5
+fi
+
 #
 #echo ">>> Launch new application."
 #
+
+
+echo ">>> DONE" > ${REPOSITORY}/test.txt
+
 #JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 #echo ">>> JAR_NAME: ${JAR_NAME}"
 #
