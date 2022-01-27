e1777d27f4377c3550e85ab4d903d8a7a1afdb6a
yologger <yologger1013@gmail.com>
2022-01-27 16:28:12 +0900

    Test CI/CD

--- src/main/java/com/yologger/heart_to_heart_api/controller/auth/AuthController.java
+++ src/main/java/com/yologger/heart_to_heart_api/controller/auth/AuthController.java
@@ -1,27 +1,27 @@
-//package com.yologger.heart_to_heart_api.controller.auth;
-//
-//import com.yologger.heart_to_heart_api.common.exception.business.UserAlreadyExistException;
-//import com.yologger.heart_to_heart_api.service.auth.AuthService;
-//import com.yologger.heart_to_heart_api.service.auth.JoinRequestDto;
-//import com.yologger.heart_to_heart_api.service.auth.JoinResponseDto;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/auth")
-//@RequiredArgsConstructor
-//@Log4j2
-//public class AuthController {
-//
-//    private final AuthService authService;
-//
-//    @PostMapping(value = "/join", consumes = "application/json", produces = "application/json")
-//    public ResponseEntity<JoinResponseDto> join(@Valid @RequestBody JoinRequestDto request) throws UserAlreadyExistException { return authService.join(request); }
-//}
+package com.yologger.heart_to_heart_api.controller.auth;
+
+import com.yologger.heart_to_heart_api.common.exception.business.UserAlreadyExistException;
+import com.yologger.heart_to_heart_api.service.auth.AuthService;
+import com.yologger.heart_to_heart_api.service.auth.JoinRequestDto;
+import com.yologger.heart_to_heart_api.service.auth.JoinResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Log4j2
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "/join", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<JoinResponseDto> join(@Valid @RequestBody JoinRequestDto request) throws UserAlreadyExistException { return authService.join(request); }
+}
--- src/main/java/com/yologger/heart_to_heart_api/service/auth/JoinRequestDto.java
+++ src/main/java/com/yologger/heart_to_heart_api/service/auth/JoinRequestDto.java
@@ -1,49 +1,49 @@
-//package com.yologger.heart_to_heart_api.service.auth;
-//
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.yologger.heart_to_heart_api.repository.user.UserEntity;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.Pattern;
-//
-//@Builder
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class JoinRequestDto {
-//
-//    @NotBlank(message = "email must not be empty.")
-//    @Email(message = "email must be in email format.")
-//    @JsonProperty(value = "email")
-//    private String email;
-//
-//    @NotBlank(message = "name must not be empty.")
-//    @JsonProperty(value = "name")
-//    private String name;
-//
-//    @NotBlank(message = "'nickname must not be empty.")
-//    @JsonProperty(value = "nickname")
-//    private String nickname;
-//
-//    @NotBlank(message = "password must not be empty.")
-//    @Pattern(
-//            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
-//            message = "'password must contain at least one uppercase letter, lowercase letter, and special character, and can have a minimum of 8 characters and a maximum of 20 characters."
-//    )
-//    @JsonProperty(value = "password")
-//    private String password;
-//
-//    public UserEntity toEntity() {
-//        return UserEntity.builder()
-//                .email(this.email)
-//                .name(this.name)
-//                .nickname(this.nickname)
-//                .password(this.password)
-//                .build();
-//    }
-//}
+package com.yologger.heart_to_heart_api.service.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yologger.heart_to_heart_api.repository.user.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinRequestDto {
+
+    @NotBlank(message = "email must not be empty.")
+    @Email(message = "email must be in email format.")
+    @JsonProperty(value = "email")
+    private String email;
+
+    @NotBlank(message = "name must not be empty.")
+    @JsonProperty(value = "name")
+    private String name;
+
+    @NotBlank(message = "'nickname must not be empty.")
+    @JsonProperty(value = "nickname")
+    private String nickname;
+
+    @NotBlank(message = "password must not be empty.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+            message = "'password must contain at least one uppercase letter, lowercase letter, and special character, and can have a minimum of 8 characters and a maximum of 20 characters."
+    )
+    @JsonProperty(value = "password")
+    private String password;
+
+    public UserEntity toEntity() {
+        return UserEntity.builder()
+                .email(this.email)
+                .name(this.name)
+                .nickname(this.nickname)
+                .password(this.password)
+                .build();
+    }
+}
