340fab9b25e88b84f3a7334756e7ab2d8a9b6fe3
yologger <yologger1013@gmail.com>
2022-01-27 17:34:26 +0900

    Test CI/CD

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,12 +7,13 @@ echo -e "[$(date "+%Y-%m-%d %I:%M:%S")] Copy jar to project root." > $REPOSITORY
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
-if [ -z "$CURRENT_PID" ]; then
+if [ -z $CURRENT_PID ]
+then
   echo "[$(date "+%Y-%m-%d %I:%M:%S")] There is no previously running application." >> $REPOSITORY/deploy_log.txt
 else
   echo "[$(date "+%Y-%m-%d %I:%M:%S")] A previously running application exists." >> $REPOSITORY/deploy_log.txt
   echo "[$(date "+%Y-%m-%d %I:%M:%S")] kill -15 $CURRENT_PID" >> $REPOSITORY/deploy_log.txt
-  kill -15 "$CURRENT_PID"
+  sudo kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
@@ -20,7 +21,6 @@ echo "[$(date "+%Y-%m-%d %I:%M:%S")] Launch new application." >> $REPOSITORY/dep
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 echo "[$(date "+%Y-%m-%d %I:%M:%S")] JAR_NAME: ${JAR_NAME}" >> $REPOSITORY/deploy_log.txt
 
-nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" 2>&1 &
-echo "[$(date "+%Y-%m-%d %I:%M:%S")] Server Started." >> $REPOSITORY/deploy_log.txt
-
+nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" > /dev/null 2> /dev/null < /dev/null &
+#nohup java -Dspring.profiles.active=prod -jar $REPOSITORY/"$JAR_NAME" 2>&1 &
 # nohup java -Dspring.profiles.active=prod -jar /home/ec2-user/app/heart-to-heart-api/heart-to-heart-api-0.0.1.jar 2>&1 &
