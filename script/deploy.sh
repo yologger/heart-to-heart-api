19ef0a15db07b6ad36f8076cedad87c72b8223dd
yologger <yologger1013@gmail.com>
2022-03-14 15:57:17 +0900

    Test CI/CD pipeline

--- script/deploy.sh
+++ script/deploy.sh
@@ -26,9 +26,10 @@ chmod +x $REPOSITORY/$JAR_NAME
 echo "[$(date "+%Y-%m-%d %I:%M:%S")] Launch new application."
 
 # nohup java -Dspring.config.location=/home/ec2-user/app/heart-to-heart-api/application-prod.properties -Dspring.profiles.active=prod -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
 if [ "$DEPLOYMENT_GROUP_NAME" == "h2h-code-deploy-group-dev" ]
 then
-    nohup java -Dspring.config.location=/home/ec2-user/app/heart-to-heart-api/application-prod.properties -Dspring.profiles.active=dev -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    nohup java -Dspring.config.location=/home/ec2-user/app/heart-to-heart-api/application-dev.properties -Dspring.profiles.active=dev -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "h2h-code-deploy-group-prod" ]
