a0e396810d0c3c347c7f88dee31c9ef3cb935a8c
yologger <yologger1013@gmail.com>
2022-04-29 21:55:32 +0900

    test: ArgoCD blue/green 배포

--- src/main/java/com/yologger/heart_to_heart_api/config/SecurityConfiguration.java
+++ src/main/java/com/yologger/heart_to_heart_api/config/SecurityConfiguration.java
@@ -39,7 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             "/auth/reissueToken",
             "/test/test1",
             "/test/test2",
-            "/test/test3"
+            "/test/test3",
+            "/test/test4",
     );
 
     @Bean
