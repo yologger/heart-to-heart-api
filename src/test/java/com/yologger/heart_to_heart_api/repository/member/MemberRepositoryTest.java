4476cbfd50118e7d9580ecb9495f12048058c6f4
yologger <yologger1013@gmail.com>
2022-05-12 21:43:47 +0900

    test: Data layer 테스트 코드 추가

--- src/test/java/com/yologger/heart_to_heart_api/repository/member/MemberRepositoryTest.java
+++ src/test/java/com/yologger/heart_to_heart_api/repository/member/MemberRepositoryTest.java
@@ -1,80 +1,84 @@
 package com.yologger.heart_to_heart_api.repository.member;
 
-import com.yologger.heart_to_heart_api.repository.post.PostRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.truth.Truth.assertThat;
 
 @DataJpaTest
 @DisplayName("MemberRepository 테스트")
+@Import(MemberRepositoryTestConfig.class)
 class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Autowired
-    private PostRepository postRepository;
-
-//    @BeforeEach
-//    void setUp() {
-//    }
+    @BeforeEach
+    void setUp() {
+    }
 
     @AfterEach
     public void tearDown() {
         memberRepository.deleteAll();
     }
-//
-//    @Test
-//    @Ignore
-//    @DisplayName("사용자 추가 및 전체 조회하기 테스트")
-//    public void test_queryMember() {
-//
-//        // Given
-//        String email = "CR7@gmail.com";
-//        String name = "Cristiano Ronaldo";
-//        String password = "12341234";
-//        String nickname = "CR7";
-//
-//        MemberEntity input = MemberEntity.builder()
-//                .email(email)
-//                .name(name)
-//                .password(password)
-//                .nickname(nickname)
-//                .build();
-//
-//        memberRepository.save(input);
-//
-//        // When
-//        List<MemberEntity> members = memberRepository.findAll();
-//
-//        // Then
-//        assertThat(members.size()).isEqualTo(1);
-//    }
-//
-//    @Test
-//    @DisplayName("이메일로 사용자 조회하기 테스트")
-//    public void test_findByEmail() {
-//        // Given
-//        String email = "CR7@gmail.com";
-//        String name = "Cristiano Ronaldo";
-//        String password = "12341234";
-//        String nickname = "CR7";
-//
-//        MemberEntity input = MemberEntity.builder()
-//                .email(email)
-//                .name(name)
-//                .password(password)
-//                .nickname(nickname)
-//                .build();
-//
-//        memberRepository.save(input);
-//
-//        // When
-//        Optional<MemberEntity> output = memberRepository.findByEmail(email);
-//
-//        // Then
-//        assertTrue(output.isPresent());
-//        assertThat(output.get().getEmail()).isEqualTo(email);
-//    }
+
+    @Test
+    @DisplayName("사용자 추가 및 전체 조회하기 테스트")
+    public void test_queryMember() {
+
+        // Given
+        String dummyEmail = "CR7@gmail.com";
+        String dummyName = "Cristiano Ronaldo";
+        String dummyPassword = "12341234";
+        String dummyNickname = "CR7";
+
+        MemberEntity input = MemberEntity.builder()
+                .email(dummyEmail)
+                .name(dummyName)
+                .password(dummyPassword)
+                .nickname(dummyNickname)
+                .build();
+
+        memberRepository.save(input);
+
+        // When
+        List<MemberEntity> members = memberRepository.findAll();
+
+        // Then
+        assertThat(members.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("이메일로 사용자 조회하기 테스트")
+    public void test_findByEmail() {
+        // Given
+        String email = "CR7@gmail.com";
+        String name = "Cristiano Ronaldo";
+        String password = "12341234";
+        String nickname = "CR7";
+
+        MemberEntity input = MemberEntity.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        memberRepository.save(input);
+
+        // When
+        Optional<MemberEntity> output = memberRepository.findByEmail(email);
+
+        // Then
+        assertThat(output.isPresent()).isTrue();
+        assertThat(output.get().getEmail()).isEqualTo(email);
+    }
 }
--- src/test/java/com/yologger/heart_to_heart_api/repository/post/PostRepositoryTest.java
+++ src/test/java/com/yologger/heart_to_heart_api/repository/post/PostRepositoryTest.java
@@ -1,15 +1,27 @@
 package com.yologger.heart_to_heart_api.repository.post;
 
 
+import com.yologger.heart_to_heart_api.repository.member.MemberEntity;
 import com.yologger.heart_to_heart_api.repository.member.MemberRepository;
+import com.yologger.heart_to_heart_api.repository.post_image.PostImageEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.google.common.truth.Truth.assertThat;
 
 @DataJpaTest
 @DisplayName("PostRepository 테스트")
+@Import(PostRepositoryTestConfig.class)
 class PostRepositoryTest {
 
     @Autowired
@@ -29,46 +41,51 @@ class PostRepositoryTest {
         memberRepository.deleteAll();
     }
 
-//    @Test
-//    @Ignore
-//    @DisplayName("새로운 포스트 작성 및 조회 테스트")
-//    public void savePost() {
-//        // Given
-//        String email = "yologger1013@gmail.com";
-//        String name = "yologger";
-//        String nickname = "yologger";
-//        String password = "1234Qwer!";
-//
-//        MemberEntity writer = MemberEntity.builder()
-//                .email(email)
-//                .nickname(nickname)
-//                .name(name)
-//                .password(password)
-//                .build();
-//
-//        MemberEntity savedWriter = memberRepository.save(writer);
-//
-//        // When
-//        String content = "content";
-//
-//        PostEntity newPost = PostEntity.builder()
-//                .content(content)
-//                .writer(savedWriter)
-//                .build();
-//
-//        postRepository.save(newPost);
-//        Optional<PostEntity> savedPost = postRepository.findById(savedWriter.getId());
-//
-//        List<String> imageUrlStrings = Arrays.asList("/123123", "1231233");
-//        List<PostImageEntity> postImageEntities = imageUrlStrings.stream().map(i -> PostImageEntity.builder().imageUrl(i).post(savedPost.get()).build()).collect(Collectors.toList());
-//
-//        postImageEntities.forEach(p -> {
-//            p.setPost(savedPost.get());
-//        });
-//
-//        // Then
-//        assertThat(savedPost.isPresent()).isTrue();
-//        assertThat(savedPost.get().getContent()).isEqualTo(content);
-//        assertThat(savedPost.get().getWriter().getEmail()).isEqualTo("yologger1013@gmail.com");
-//    }
+    @Test
+    @DisplayName("새로운 포스트 작성 및 조회 테스트")
+    public void savePost() {
+
+        // Given
+        String email = "yologger1013@gmail.com";
+        String name = "yologger";
+        String nickname = "yologger";
+        String password = "1234Qwer!";
+
+        MemberEntity writer = MemberEntity.builder()
+                .email(email)
+                .nickname(nickname)
+                .name(name)
+                .password(password)
+                .build();
+
+        MemberEntity savedWriter = memberRepository.save(writer);
+
+        // When
+        String content = "content";
+
+        PostEntity newPost = PostEntity.builder()
+                .content(content)
+                .writer(savedWriter)
+                .build();
+
+        postRepository.save(newPost);
+        Optional<PostEntity> savedPost = postRepository.findById(savedWriter.getId());
+
+        List<String> imageUrlStrings = Arrays.asList("/test/1.png", "/test/2.png");
+        List<PostImageEntity> postImageEntities = imageUrlStrings.stream()
+                .map(i -> PostImageEntity.builder()
+                        .imageUrl(i)
+                        .post(savedPost.get())
+                        .build()
+                ).collect(Collectors.toList());
+
+        postImageEntities.forEach(p -> {
+            p.setPost(savedPost.get());
+        });
+
+        // Then
+        assertThat(savedPost.isPresent()).isTrue();
+        assertThat(savedPost.get().getContent()).isEqualTo(content);
+        assertThat(savedPost.get().getWriter().getEmail()).isEqualTo("yologger1013@gmail.com");
+    }
 }
