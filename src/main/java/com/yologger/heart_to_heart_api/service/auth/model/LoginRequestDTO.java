7b48af6a60e077e132b39c67cb1edc2f7175fe0e
yologger <yologger1013@gmail.com>
2022-06-13 09:10:04 +0900

    [DOCS] 로그인 API 문서화

--- src/main/java/com/yologger/heart_to_heart_api/service/auth/model/LoginRequestDTO.java
+++ src/main/java/com/yologger/heart_to_heart_api/service/auth/model/LoginRequestDTO.java
@@ -1,6 +1,8 @@
 package com.yologger.heart_to_heart_api.service.auth.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +16,16 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ApiModel(
+        value = "로그인 요청 모델",
+        description = "email, password를 포함하는 로그인 요청 모델"
+)
 public class LoginRequestDTO {
 
     @NotBlank(message = "email must not be empty.")
     @Email(message = "email must be in email format.")
     @JsonProperty(value = "email")
+    @ApiModelProperty(value = "이메일", example = "ronaldo@gmail.com")
     private String email;
 
     @NotBlank(message = "password must not be empty.")
@@ -27,5 +34,6 @@ public class LoginRequestDTO {
             message = "'password must contain at least one uppercase letter, lowercase letter, and special character, and can have a minimum of 8 characters and a maximum of 20 characters."
     )
     @JsonProperty(value = "password")
+    @ApiModelProperty(value = "비밀번호")
     private String password;
 }
