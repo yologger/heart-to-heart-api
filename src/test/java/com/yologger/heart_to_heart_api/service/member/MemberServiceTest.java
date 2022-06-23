7f65a6da2462e22cac0faaf756421801d94c2720
yologger <yologger1013@gmail.com>
2022-06-23 17:33:07 +0900

    [TEST] 테스트 코드 추가 - 사용자가 존재하지 않을 때 글 작성 실패 테스트

--- src/test/java/com/yologger/heart_to_heart_api/service/member/MemberServiceTest.java
+++ src/test/java/com/yologger/heart_to_heart_api/service/member/MemberServiceTest.java
@@ -1,7 +1,6 @@
 package com.yologger.heart_to_heart_api.service.member;
 
 import com.yologger.heart_to_heart_api.config.TestAwsS3Config;
-import com.yologger.heart_to_heart_api.repository.member.MemberRepository;
 import io.findify.s3mock.S3Mock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +10,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,8 +21,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class MemberServiceTest {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     private MemberService memberService;
@@ -39,8 +38,4 @@ class MemberServiceTest {
         s3Mock.stop();
     }
 
-    @DisplayName("글 작성 테스트")
-    public void test_write_post() {
-
-    }
 }
--- src/test/java/com/yologger/heart_to_heart_api/service/post/PostServiceTest.java
+++ src/test/java/com/yologger/heart_to_heart_api/service/post/PostServiceTest.java
@@ -1,10 +1,13 @@
 package com.yologger.heart_to_heart_api.service.post;
 
 import com.yologger.heart_to_heart_api.config.TestAwsS3Config;
+import com.yologger.heart_to_heart_api.controller.post.exception.InvalidWriterIdException;
 import com.yologger.heart_to_heart_api.service.post.model.GetPostsResponseDTO;
+import com.yologger.heart_to_heart_api.service.post.model.RegisterPostRequestDTO;
 import io.findify.s3mock.S3Mock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,7 @@ import org.springframework.test.context.jdbc.Sql;
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,20 +38,41 @@ class PostServiceTest {
         s3Mock.stop();
     }
 
-    @Test
+    @Nested
+    @DisplayName("글 작성 테스트")
+    class WritePostTest {
+        @Test
+        @DisplayName("실패 테스트 - 사용자가 존재하지 않을 때")
+        public void writePost_whenUserNotExists() {
+            RegisterPostRequestDTO request = RegisterPostRequestDTO.builder()
+                    .memberId(1L)
+                    .content("content")
+                    .build();
+
+            assertThatThrownBy(() -> postService.registerPost(request))
+                    .isInstanceOf(InvalidWriterIdException.class);
+        }
+    }
+
+    @Nested
     @DisplayName("글 조회 테스트")
-    @Sql(scripts = {
-            "classpath:sql/dummy/users.sql",
-            "classpath:sql/dummy/posts.sql",
-            "classpath:sql/dummy/post_images.sql"
-    })
-    void test_query_posts() {
-        Long dummyMemberId = 0L;
-        Integer dummyPage = 0;
-        Integer dummySize = 10;
+    class GetPostsTest {
 
-        GetPostsResponseDTO response = postService.getAllPosts(dummyMemberId, dummyPage, dummySize);
+        @Test
+        @DisplayName("글 조회 성공 테스트")
+        @Sql(scripts = {
+                "classpath:sql/dummy/users.sql",
+                "classpath:sql/dummy/posts.sql",
+                "classpath:sql/dummy/post_images.sql"
+        })
+        void test_query_posts() {
+            Long dummyMemberId = 0L;
+            Integer dummyPage = 0;
+            Integer dummySize = 10;
 
-        assertThat(response.getSize()).isNotZero();
+            GetPostsResponseDTO response = postService.getAllPosts(dummyMemberId, dummyPage, dummySize);
+
+            assertThat(response.getSize()).isNotZero();
+        }
     }
 }
