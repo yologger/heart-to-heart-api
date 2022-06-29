3619ebfa976ca2bc5b57db932678eef6b3bd5ed3
yologger <yologger1013@gmail.com>
2022-06-29 20:13:19 +0900

    [TEST] 회원 탈퇴 테스트 코드 추가

--- src/test/java/com/yologger/heart_to_heart_api/service/member/MemberServiceTest.java
+++ src/test/java/com/yologger/heart_to_heart_api/service/member/MemberServiceTest.java
@@ -1,18 +1,25 @@
 package com.yologger.heart_to_heart_api.service.member;
 
 import com.yologger.heart_to_heart_api.config.TestAwsS3Config;
+import com.yologger.heart_to_heart_api.service.member.model.DeleteAccountResponseDTO;
 import io.findify.s3mock.S3Mock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -38,4 +45,18 @@ class MemberServiceTest {
         s3Mock.stop();
     }
 
+    @DisplayName("회원 탈퇴 테스트")
+    @Sql(scripts = "classpath:sql/dummy/users.sql")
+    @Nested
+    public class DeleteAccountTest {
+        @Test
+        @DisplayName("회원 탈퇴 성공 테스트")
+        public void deleteAccount_success() {
+            Long targetId = 1L;
+            assertThatNoException().isThrownBy(() -> {
+                DeleteAccountResponseDTO response = memberService.deleteAccount(targetId);
+                assertThat(response.getMessage()).isEqualTo("deleted.");
+            });
+        }
+    }
 }
--- src/test/java/com/yologger/heart_to_heart_api/service/user/UserServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.yologger.heart_to_heart_api.service.user;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("UserService 테스트")
-class UserServiceTest {
-
-
-}
